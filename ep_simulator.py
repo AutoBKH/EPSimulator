abda3cf47e7ce740818724e6b0abb7d39d871c8b
pablo <pcusnir@gmail.com>
2020-03-22 23:48:05 +0200

    WIP

--- ep_simulator.py
+++ ep_simulator.py
@@ -21,5 +21,40 @@ def publish():
         return f"called publish with {request.method}"
 
 
+@app.route('/config', methods=['POST', 'GET'])
+def config():
+    if request.method == 'POST':
+        if request.is_json:
+            print(f"Changing configuration")
+            return request.json
+        else:
+            return str(request.data, "utf-8")
+    elif request.method == 'GET':
+        return f"The current configuration is: "
+
+
+@app.route('/start', methods=['POST'])
+def start_simulator():
+    if request.method == 'POST':
+        return f"Starting simulator"
+    else:
+        return f"Unrecognized request"
+
+
+@app.route('/stop', methods=['POST'])
+def stop_simulator():
+    if request.method == 'POST':
+        return f"Stopping simulator"
+    else:
+        return f"Unrecognized request"
+
+
+@app.route('/restart', methods=['POST'])
+def restart_simulator():
+    if request.method == 'POST':
+        return f"Stopping simulator"
+    else:
+        return f"Unrecognized request"
+
 if __name__ == "__main__":
     app.run(debug=True)
