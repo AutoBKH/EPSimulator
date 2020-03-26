a2db0c1d4c4be6bbdef59a9a902467b366cc5cc3
pablo <pcusnir@gmail.com>
2020-03-26 23:10:36 +0200

    Removed redundant code

--- ep_simulator.py
+++ ep_simulator.py
@@ -4,36 +4,9 @@ import jsonpickle
 app = Flask(__name__)
 
 
-@app.route('/')
-def sanity():
+def is_alive():
     return 'EP Simulator is up and running\n'
 
-
-@app.route('/publish', methods=['POST'])
-def publish():
-    if request.method == 'POST':
-        if request.is_json:
-            return request.json
-        else:
-            return str(request.data, "utf-8")
-    else:
-        return f"called publish with {request.method}"
-
-
-# @app.route('/config', methods=['POST', 'GET'])
-def config():
-    if request.method == 'POST':
-        if request.is_json:
-            print(f"Changing configuration")
-            return request.json
-        else:
-            return str(request.data, "utf-8")
-    elif request.method == 'GET':
-        return f"The current configuration is: "
-
-
-def about():
-    return "about page"
 
 def send_event():
     if request.is_json:
@@ -46,36 +19,20 @@ def send_event():
                        send_to=unpickle["send_to"])
 
 
-app.add_url_rule('/config', view_func=config, methods=['POST', 'GET'])
-app.add_url_rule('/about', view_func=about, methods=['POST', 'GET'])
+def publish():
+    if request.method == 'POST':
+        if request.is_json:
+            return request.json
+        else:
+            return str(request.data, "utf-8")
+    else:
+        return f"called publish with {request.method}"
+
+
+app.add_url_rule('/', view_func=is_alive)
+app.add_url_rule('/is_alive', view_func=is_alive)
 app.add_url_rule('/send_event', view_func=send_event, methods=['POST'])
-
-# app.view_functions['about'] = about
-
-
-
-@app.route('/start', methods=['POST'])
-def start_simulator():
-    if request.method == 'POST':
-        return f"Starting simulator"
-    else:
-        return f"Unrecognized request"
-
-
-@app.route('/stop', methods=['POST'])
-def stop_simulator():
-    if request.method == 'POST':
-        return f"Stopping simulator"
-    else:
-        return f"Unrecognized request"
-
-
-@app.route('/restart', methods=['POST'])
-def restart_simulator():
-    if request.method == 'POST':
-        return f"Restarted simulator"
-    else:
-        return f"Unrecognized request"
+app.add_url_rule('/publish', view_func=publish, methods=['POST'])
 
 
 if __name__ == "__main__":
